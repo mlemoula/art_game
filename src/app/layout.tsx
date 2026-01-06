55b4d2bda6503b015fd6ceb28917dfc50c39ab25
Martin Lemoulant <martin.lemoulant@gmail.com>
2026-01-06 17:45:00 +0100

    improving sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/context/theme";
-export { metadata, generateMetadata } from "./metadata";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
