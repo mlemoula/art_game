501cf9594c93d3a966a7278af763dc1acc7dc7f9
Martin Lemoulant <martin.lemoulant@gmail.com>
2026-01-06 17:06:58 +0100

    improving sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/context/theme";
+import Head from "./head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,11 +15,14 @@ const geistMono = Geist_Mono({
 
 export default function RootLayout({
   children,
+  searchParams = {},
 }: Readonly<{
   children: React.ReactNode;
+  searchParams?: Record<string, string | string[] | undefined>;
 }>) {
   return (
     <html lang="en">
+      <Head searchParams={searchParams} />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
